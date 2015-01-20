05870417ea6d0bcc66c08640fc033f3d56a9218c
Ryan Lagoy <rlagoy@gmail.com>
2015-01-20 18:52:24 -0500

    Changes To SignalViewer

--- GNURadio_SDR/SignalViewer.py
+++ GNURadio_SDR/SignalViewer.py
@@ -2,7 +2,7 @@
 ##################################################
 # Gnuradio Python Flow Graph
 # Title: Comms Signal Viewer
-# Generated: Sun Jan 18 23:17:46 2015
+# Generated: Mon Jan 19 12:45:06 2015
 ##################################################
 
 from PyQt4 import Qt
@@ -134,12 +134,6 @@ class SignalViewer(gr.top_block, Qt.QWidget):
                 taps=None,
                 fractional_bw=None,
         )
-        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
-                interpolation=1,
-                decimation=4,
-                taps=None,
-                fractional_bw=None,
-        )
         self.qtgui_waterfall_sink_x_0 = qtgui.waterfall_sink_c(
         	1024, #size
         	firdes.WIN_BLACKMAN_hARRIS, #wintype
@@ -172,6 +166,51 @@ class SignalViewer(gr.top_block, Qt.QWidget):
         
         self._qtgui_waterfall_sink_x_0_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_0.pyqwidget(), Qt.QWidget)
         self.main_tab_layout_1.addWidget(self._qtgui_waterfall_sink_x_0_win)
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_c(
+        	1024, #size
+        	samp_rate, #samp_rate
+        	"", #name
+        	1 #number of inputs
+        )
+        self.qtgui_time_sink_x_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
+        
+        self.qtgui_time_sink_x_0.set_y_label("Amplitude", "")
+        
+        self.qtgui_time_sink_x_0.enable_tags(-1, True)
+        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0.enable_grid(False)
+        
+        labels = ["", "", "", "", "",
+                  "", "", "", "", ""]
+        widths = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+                  "magenta", "yellow", "dark red", "dark green", "blue"]
+        styles = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+                   -1, -1, -1, -1, -1]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+        
+        for i in xrange(2*1):
+            if len(labels[i]) == 0:
+                if(i % 2 == 0):
+                    self.qtgui_time_sink_x_0.set_line_label(i, "Re{{Data {0}}}".format(i/2))
+                else:
+                    self.qtgui_time_sink_x_0.set_line_label(i, "Im{{Data {0}}}".format(i/2))
+            else:
+                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+        
+        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
         self.qtgui_number_sink_0 = qtgui.number_sink(
                 gr.sizeof_char,
                 0,
@@ -242,8 +281,6 @@ class SignalViewer(gr.top_block, Qt.QWidget):
         self.main_tab_layout_0.addWidget(self._qtgui_freq_sink_x_0_win)
         self.low_pass_filter_0_0 = filter.fir_filter_ccf(1, firdes.low_pass(
         	1, samp_rate, 100e3, 1e6, firdes.WIN_HAMMING, 6.76))
-        self.low_pass_filter_0 = filter.fir_filter_ccf(1, firdes.low_pass(
-        	1, samp_rate, 100e3, 1e6, firdes.WIN_HAMMING, 6.76))
         _fskDemodBool_check_box = Qt.QCheckBox("Don't FSK Demod")
         self._fskDemodBool_choices = {True: True, False: False}
         self._fskDemodBool_choices_inv = dict((v,k) for k,v in self._fskDemodBool_choices.iteritems())
@@ -287,16 +324,15 @@ class SignalViewer(gr.top_block, Qt.QWidget):
         self.connect((self.digital_gfsk_demod_0, 0), (self.fskData, 0))    
         self.connect((self.digital_gfsk_demod_0, 0), (self.qtgui_number_sink_0, 0))    
         self.connect((self.fmDemod, 0), (self.rational_resampler_xxx_0_0, 0))    
-        self.connect((self.fskDemod, 0), (self.rational_resampler_xxx_0, 0))    
-        self.connect((self.low_pass_filter_0, 0), (self.digital_gfsk_demod_0, 0))    
+        self.connect((self.fskDemod, 0), (self.digital_gfsk_demod_0, 0))    
         self.connect((self.low_pass_filter_0_0, 0), (self.analog_wfm_rcv_0, 0))    
-        self.connect((self.rational_resampler_xxx_0, 0), (self.low_pass_filter_0, 0))    
         self.connect((self.rational_resampler_xxx_0_0, 0), (self.low_pass_filter_0_0, 0))    
         self.connect((self.rational_resampler_xxx_0_0_0, 0), (self.volumeBlock, 0))    
         self.connect((self.recordIQ, 0), (self.IQData, 0))    
         self.connect((self.rtlsdr_source_0, 0), (self.fmDemod, 0))    
         self.connect((self.rtlsdr_source_0, 0), (self.fskDemod, 0))    
         self.connect((self.rtlsdr_source_0, 0), (self.qtgui_freq_sink_x_0, 0))    
+        self.connect((self.rtlsdr_source_0, 0), (self.qtgui_time_sink_x_0, 0))    
         self.connect((self.rtlsdr_source_0, 0), (self.qtgui_waterfall_sink_x_0, 0))    
         self.connect((self.rtlsdr_source_0, 0), (self.recordIQ, 0))    
         self.connect((self.volumeBlock, 0), (self.audio_sink_0, 0))    
@@ -311,46 +347,46 @@ class SignalViewer(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.qtgui_waterfall_sink_x_0.set_frequency_range(self.freq, self.samp_rate)
-        self.qtgui_freq_sink_x_0.set_frequency_range(self.freq, self.samp_rate)
-        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 100e3, 1e6, firdes.WIN_HAMMING, 6.76))
         self.low_pass_filter_0_0.set_taps(firdes.low_pass(1, self.samp_rate, 100e3, 1e6, firdes.WIN_HAMMING, 6.76))
-        self.rtlsdr_source_0.set_sample_rate(self.samp_rate)
+        self.qtgui_freq_sink_x_0.set_frequency_range(self.freq, self.samp_rate)
+        self.qtgui_waterfall_sink_x_0.set_frequency_range(self.freq, self.samp_rate)
         Qt.QMetaObject.invokeMethod(self._samp_rate_line_edit, "setText", Qt.Q_ARG("QString", eng_notation.num_to_str(self.samp_rate)))
+        self.rtlsdr_source_0.set_sample_rate(self.samp_rate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
 
     def get_recordBool(self):
         return self.recordBool
 
     def set_recordBool(self, recordBool):
         self.recordBool = recordBool
-        self.recordIQ.set_open(bool(self.recordBool))
         self._recordBool_callback(self.recordBool)
+        self.recordIQ.set_open(bool(self.recordBool))
 
     def get_fskDemodBool(self):
         return self.fskDemodBool
 
     def set_fskDemodBool(self, fskDemodBool):
         self.fskDemodBool = fskDemodBool
-        self.fskDemod.set_open(bool(self.fskDemodBool))
         self._fskDemodBool_callback(self.fskDemodBool)
+        self.fskDemod.set_open(bool(self.fskDemodBool))
 
     def get_freq(self):
         return self.freq
 
     def set_freq(self, freq):
         self.freq = freq
-        self.qtgui_waterfall_sink_x_0.set_frequency_range(self.freq, self.samp_rate)
         self.qtgui_freq_sink_x_0.set_frequency_range(self.freq, self.samp_rate)
-        self.rtlsdr_source_0.set_center_freq(self.freq, 0)
+        self.qtgui_waterfall_sink_x_0.set_frequency_range(self.freq, self.samp_rate)
         Qt.QMetaObject.invokeMethod(self._freq_counter, "setValue", Qt.Q_ARG("double", self.freq))
+        self.rtlsdr_source_0.set_center_freq(self.freq, 0)
 
     def get_fmDemodBool(self):
         return self.fmDemodBool
 
     def set_fmDemodBool(self, fmDemodBool):
         self.fmDemodBool = fmDemodBool
-        self.fmDemod.set_open(bool(self.fmDemodBool))
         self._fmDemodBool_callback(self.fmDemodBool)
+        self.fmDemod.set_open(bool(self.fmDemodBool))
 
 if __name__ == '__main__':
     import ctypes
